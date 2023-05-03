4b4a37ecc3688704cb3b0be0d9d3083754ecfd41
Abduselam <aabdu2833@gmail.com>
2023-05-03 15:02:55 +0300

    with real audio

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
+import 'dart:async';
 import 'dart:math';
 import 'dart:ui' as ui;
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:just_audio_background/just_audio_background.dart';
+import 'package:just_audio/just_audio.dart';
 
-void main() {
+void main() async {
+  // await JustAudioBackground.init(
+  //   androidNotificationChannelId: 'com.ryanheise.bg_demo.channel.audio',
+  //   androidNotificationChannelName: 'Audio playback',
+  //   androidNotificationOngoing: true,
+  // );
   runApp(const MainApp());
 }
 
@@ -597,6 +605,10 @@ class _BottomPlayerState extends State<BottomPlayer> {
   double percentage = 0;
   final _counter = ValueNotifier<int>(0);
   late OpenPainter painter;
+  final player = AudioPlayer();
+  Duration? duration;
+  late StreamSubscription<Duration> positionStream;
+  bool playing = false;
   // bool i = false;
 
   @override
@@ -604,17 +616,77 @@ class _BottomPlayerState extends State<BottomPlayer> {
     painter = OpenPainter(repaint: _counter);
     // i=true;
     super.initState();
+    // chooseFile();
+    positionStream = player.positionStream.listen((event) {
+      setState(() {
+        print(event.inSeconds);
+        print(player.playerState.playing);
+        print(player.playerState.processingState.toString());
+        // if (percentage > 0) {
+        if (player.playerState.processingState == ProcessingState.completed) {
+          // player.playing;
+          playing = false;
+        }
+        painter.setProgress(
+            (event.inMilliseconds * 100) / (duration?.inMilliseconds ?? 1));
+        _counter.value++;
+        // }
+      });
+    });
+  }
+
+  void chooseFile() async {
+    print('CAlled:chooseFile');
+    try {
+      duration = await player.setAsset('assets/NutcrackerMarchLoop.mp3'
+          // 'https://freetestdata.com/wp-content/uploads/2021/09/Free_Test_Data_500KB_MP3.mp3'
+          );
+      // print(duration?.inSeconds);
+    } catch (e) {
+      print('  duration = await:$e');
+    }
+    playPause();
+  }
+
+  @override
+  void dispose() {
+    positionStream.cancel();
+    super.dispose();
+  }
+
+  void playPause() async {
+    print(player.processingState);
+    print(duration?.inSeconds);
+    if ([ProcessingState.completed, ProcessingState.idle]
+        .contains(player.processingState)) {
+      chooseFile();
+    } else {
+      try {
+        if (playing) {
+          await player.pause();
+        } else {
+          player.play(); // Play while waiting for completion
+        }
+        // print(player.playerState.playing);
+        setState(() => playing = !playing);
+      } catch (e) {
+        print('catch (e) {:$e');
+      }
+    }
   }
 
   void thefunsc(Duration sssd) {
     var a = const Duration(seconds: 0);
     var b = (50 / sssd.inMilliseconds);
+    percentage = 0;
     Future.doWhile(() {
       return Future.delayed(const Duration(milliseconds: 50), () {
         if (!mounted) {
           return false;
         }
-        print(percentage);
+        if (kDebugMode) {
+          print(percentage);
+        }
         setState(() {
           if (percentage < 99.0) {
             percentage += b * 100;
@@ -646,14 +718,23 @@ class _BottomPlayerState extends State<BottomPlayer> {
           SizedBox(
             width: MediaQuery.of(context).size.width - 48,
             height: 82,
-            child: CustomPaint(
-              painter: painter,
+            child: GestureDetector(
+              onPanStart: (details) {
+                print('START:${details.localPosition}');
+              },
+              onPanUpdate: (details) {
+                print('UPDATE:${details.localPosition}');
+              },
+              child: CustomPaint(
+                painter: painter,
+              ),
             ),
           ),
           Center(
             child: Container(
               height: 70,
               width: MediaQuery.of(context).size.width - 60,
+              // margin: EdgeInsets.all(3),
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(40),
                 color: Theme.of(context).colorScheme.secondary,
@@ -666,17 +747,22 @@ class _BottomPlayerState extends State<BottomPlayer> {
                   children: [
                     Row(
                       children: [
-                        Container(
-                          width: 38,
-                          height: 38,
-                          decoration: BoxDecoration(
-                              image: const DecorationImage(
-                                image: NetworkImage(
-                                  'https://unsplash.it/50/50',
+                        GestureDetector(
+                          onTap: () {
+                            chooseFile();
+                          },
+                          child: Container(
+                            width: 38,
+                            height: 38,
+                            decoration: BoxDecoration(
+                                image: const DecorationImage(
+                                  image: NetworkImage(
+                                    'https://unsplash.it/50/50',
+                                  ),
+                                  fit: BoxFit.cover,
                                 ),
-                                fit: BoxFit.cover,
-                              ),
-                              borderRadius: BorderRadius.circular(19)),
+                                borderRadius: BorderRadius.circular(19)),
+                          ),
                         ),
                         Padding(
                           padding: const EdgeInsets.only(left: 12),
@@ -710,9 +796,10 @@ class _BottomPlayerState extends State<BottomPlayer> {
                       ),
                       child: GestureDetector(
                           onTap: () {
-                            thefunsc(const Duration(seconds: 4));
+                            // thefunsc(const Duration(seconds: 4));
+                            playPause();
                           },
-                          child: percentage < 1 || percentage > 99
+                          child: !playing
                               ? Image.asset('assets/stop.png')
                               : Image.asset('assets/PLay.png')),
                     )
@@ -745,21 +832,30 @@ class OpenPainter extends CustomPainter {
     var paint1 = Paint()
       ..shader = ui.Gradient.linear(
         const Offset(0, 0),
-        Offset(width - (height / 2), height),
+        Offset(width, height),
         [
           const Color.fromRGBO(34, 221, 242, 0.43),
           const Color.fromRGBO(237, 27, 163, 1),
         ],
       )
-      ..color = const Color(0xff63aa65)
       ..style = PaintingStyle.stroke
       ..strokeWidth = 3;
+    var paint2 = Paint()
+      ..shader = ui.Gradient.linear(
+        const Offset(0, 0),
+        Offset(width, height),
+        [
+          const Color.fromRGBO(34, 221, 242, 0.43),
+          const Color.fromRGBO(237, 27, 163, 1),
+        ],
+      )
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 6;
     //draw arc
     double r = height / 2;
     double fullLength = (hrzLngth) + (pi * r) + (hrzLngth) + (pi * r);
     double hrzPrcnt = (hrzLngth) / fullLength;
     double vrtPrcnt = (pi * r) / fullLength;
-    double vrtLngth = vrtPrcnt * fullLength;
     // print(vrtPrcnt * 100);
     // print(Duration(seconds: 5) < Duration(seconds: 6));
     // print(
@@ -768,6 +864,15 @@ class OpenPainter extends CustomPainter {
     double k1 = (_progress * (hrzLngth) / (hrzPrcnt * 100));
     canvas.drawLine(
         Offset(r, 0), Offset(((k1 > hrzLngth ? hrzLngth : k1) + r), 0), paint1);
+    if (_progress > hrzPrcnt * 100) {
+      double k2 = ((_progress - (hrzPrcnt * 100))) / (vrtPrcnt * 2);
+      canvas.drawArc(
+          Offset(hrzLngth, 0) & Size(height, height),
+          (-25 * 3.6 * pi) / 180, //radians
+          ((k2 < 50 ? k2 : 50) * 3.6 * pi) / 180, //radians
+          false,
+          paint1);
+    }
     if (_progress > (hrzPrcnt + vrtPrcnt) * 100) {
       // print((_progress - ((hrzPrcnt + vrtPrcnt) * 100)));
       double k3 = ((_progress - ((hrzPrcnt + vrtPrcnt) * 100)) *
@@ -780,15 +885,6 @@ class OpenPainter extends CustomPainter {
           Offset(hrzLngth - (k3 > hrzLngth ? hrzLngth : k3) + r, height),
           paint1);
     }
-    if (_progress > hrzPrcnt * 100) {
-      double k2 = ((_progress - (hrzPrcnt * 100))) / (vrtPrcnt * 2);
-      canvas.drawArc(
-          Offset(hrzLngth, 0) & Size(height, height),
-          (-25 * 3.6 * pi) / 180, //radians
-          ((k2 < 50 ? k2 : 50) * 3.6 * pi) / 180, //radians
-          false,
-          paint1);
-    }
     if (_progress > (hrzPrcnt * 2 + vrtPrcnt) * 100) {
       double k4 =
           ((_progress - ((hrzPrcnt * 2 + vrtPrcnt) * 100))) / (vrtPrcnt * 2);
@@ -799,11 +895,61 @@ class OpenPainter extends CustomPainter {
           false,
           paint1);
     }
+    Offset theOffset;
+    if (_progress < hrzPrcnt * 100) {
+      theOffset = Offset(((k1 > hrzLngth ? hrzLngth : k1) + r), 0);
+    } else if (_progress < (hrzPrcnt + vrtPrcnt) * 100) {
+      double k2 = ((_progress - (hrzPrcnt * 100))) / (vrtPrcnt * 2);
+      double ang;
+      double dy;
+      double dx;
+      ang = ((k2 < 50 ? k2 : 50) * pi) / 50;
+      if (k2 < 25) {
+        dx = width + ((sin(ang) - 1) * r);
+        dy = (1 - cos(ang)) * r;
+      } else {
+        dx = width + ((sin(pi - ang) - 1) * r);
+        dy = (1 + cos(pi - ang)) * r;
+      }
+      theOffset = Offset(dx, dy);
+    } else if (_progress < (hrzPrcnt * 2 + vrtPrcnt) * 100) {
+      double k3 = ((_progress - ((hrzPrcnt + vrtPrcnt) * 100)) *
+          (hrzLngth) /
+          ((hrzPrcnt) * 100));
+      theOffset =
+          Offset(hrzLngth - (k3 > hrzLngth ? hrzLngth : k3) + r, height);
+      // canvas.drawCircle(Offset(dx, dy), 4, paint2);
+    } else {
+      double k4 =
+          ((_progress - ((hrzPrcnt * 2 + vrtPrcnt) * 100))) / (vrtPrcnt * 2);
+      double ang;
+      double dy;
+      double dx;
+      ang = ((k4 < 50 ? k4 : 50) * pi) / 50;
+      if (k4 < 25) {
+        dx = (1 - sin(ang)) * r;
+        dy = (1 + cos(ang)) * r;
+      } else {
+        dx = (1 - sin(pi - ang)) * r;
+        dy = (1 - cos(pi - ang)) * r;
+      }
+      theOffset = Offset(dx, dy);
+    }
+    canvas.drawCircle(theOffset, 3, paint2);
   }
+
+  // @override
+  // bool? hitTest(Offset position) {
+  //   print(position);
+  //   // print(position.dx);
+  //   // print(position.dy);
+  //   // TODO: implement hitTest
+  //   return super.hitTest(position);
+  // }
 
   @override
   bool shouldRepaint(CustomPainter oldDelegate) => true;
 
-  @override
-  bool shouldRebuildSemantics(CustomPainter oldDelegate) => true;
+  // @override
+  // bool shouldRebuildSemantics(CustomPainter oldDelegate) => true;
 }
